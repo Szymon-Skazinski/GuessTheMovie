d4b79c0395b9d4b99bce435d9d90fe5c93a4cc07
Szymon Skazinski <szyskazi@gmail.com>
2020-04-11 22:19:19 +0200

    Delete 'isHidden'

--- src/Game/Game.java
+++ src/Game/Game.java
@@ -7,24 +7,22 @@ public class Game {
 
     private String movieName;
     private String hiddenName;          //underscored movieName
-    private boolean isHidden;
-    private String wrongGuess;
-    private int wrongGuessCount;
+    private String wrongGuess;          //String storing wrong char inputed by user
+    private int wrongGuessCount;        //count of uniqe char in wrongGuess
+    private int point;
 
     public Game(String movieName) {
         this.movieName = movieName;
+        point = 10;
         wrongGuessCount = 0;
         wrongGuess = "";
-        isHidden = true;
+        hiddenName = "";
         gameStart();
-
     }
 
 
     void gameStart() {
         int movieNameLength = movieName.length();
-        wrongGuess = "";
-        hiddenName = "";
 
         for (int i = 0; i < movieNameLength; i++) {                         // creating hiddenName (underscored)
             if (isWhitespace(movieName.charAt(i))) hiddenName += ' ';       //checkin if its space
@@ -33,18 +31,19 @@ public class Game {
 
         System.out.println(hiddenName);
 
-        while (isHidden == true) {
+        while (point>0 && !(hiddenName.equalsIgnoreCase(movieName)) ) {
             letterReveal();
         }
 
         if (hiddenName.equalsIgnoreCase(movieName)) {
-        System.out.println("You win");
+        System.out.println("You win! Your score is: " + point);
         System.out.println("You have guessed '" + movieName + "' correctly");
+        } else if(point == 0){
+           System.out.println("You have defeated");
         }
     }
 
     void letterReveal() {
-
         System.out.print("Guess a letter: ");
         Scanner scanner = new Scanner(System.in);
 
@@ -53,24 +52,22 @@ public class Game {
 
         int index = 0;
         if (movieName.indexOf(letter)>=0) {
-            while (movieName.indexOf(letter, index) >= 0) {     //checking each occurrence of letter
+            while (movieName.indexOf(letter, index) >= 0) {     //checking each occurrence of correct letter
                 index = movieName.indexOf(letter, index);
                 if (index >= 0) {
                     hiddenName = hiddenName.substring(0, index) + letter + hiddenName.substring(index + 1, hiddenName.length());
                 }
                 index++;
             }
-        } else if (wrongGuess.indexOf(letter) < 0) {
+        } else if (wrongGuess.indexOf(letter) < 0) {        //checking if wrong letter exist in wrongGuess
             wrongGuess = wrongGuess + " " +letter;
             wrongGuessCount ++;
+            point --;
         }
 
         System.out.println("You are guessing:" + hiddenName);
         System.out.println("You have guessed (" + wrongGuessCount + ") wrong letters:" + wrongGuess);
 
-        if (hiddenName.indexOf('_') <0){
-            isHidden = false;
-        }
 
     }
 
